e5f985a015cd2c9915d1496bdaefe20798892295
Bin Jiang <bin.jiang@heyario.com>
2025-08-08 13:44:09 -0700

    fix timeout

--- src/server/proxy-service.ts
+++ src/server/proxy-service.ts
@@ -130,7 +130,7 @@ export class ProxyService {
         headers: upstreamHeaders,
         params: request.query,
         data: contentToSend,
-        timeout: 3000000, // 5 minutes
+        timeout: 300_000, // 5 minutes
         responseType: 'arraybuffer',
         validateStatus: () => true, // Don't throw on non-2xx status codes
       };
